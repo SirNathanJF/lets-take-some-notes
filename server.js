a13032df8c30a4e36a2d371c479b593d42c306a1
Nathan Flessner <nathanflessner1@gmail.com>
2021-09-03 19:29:35 -0600

    added get method for notes.html

--- server.js
+++ server.js
@@ -14,3 +14,8 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
