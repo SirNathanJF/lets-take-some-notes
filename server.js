f0348b086d85a080f21f7decfc0c65b13785a707
Nathan Flessner <nathanflessner1@gmail.com>
2021-09-03 19:35:15 -0600

    added post method to notes, added the fallback route for invalid route

--- server.js
+++ server.js
@@ -22,3 +22,24 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', (req, res) => {
     res.json(db)
 })
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body
+
+    newNote.id = newNote.title.replace(/\s+/g, "").toLowerCase()
+
+    console.log(newNote)
+
+    db.push(newNote)
+
+    res.json(newNote)
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(PORT, (err) => {
+    if (err) console.error(err);
+    console.log(`App listening on PORT ${PORT}.`)
+})
