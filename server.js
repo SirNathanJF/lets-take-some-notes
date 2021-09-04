42c877441d2eb544e5e852b4ba3b745133400f29
Nathan Flessner <nathanflessner1@gmail.com>
2021-09-03 19:26:09 -0600

    added server.js file and defined requirement variables and app.x() variables

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const db = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
